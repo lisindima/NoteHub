11e654490338026df0c462833dbdc6a8e9403473
lisindima <lisindima1996@gmail.com>
2020-07-07 15:46:00 +0700

    Первый коммит

--- Shared/CloudNoteApp.swift
+++ Shared/CloudNoteApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct CloudNoteApp: App {
+    
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    #endif
+    
     var body: some Scene {
         WindowGroup {
+            #if os(iOS)
+            if horizontalSizeClass == .compact {
+                TasksView()
+            } else {
+                ContentView()
+            }
+            
+            #else
             ContentView()
+            #endif
         }
     }
 }
