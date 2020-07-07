b762c2877cf746161c0290aaaf7510cc115283f6
lisindima <lisindima1996@gmail.com>
2020-07-07 14:08:54 +0700

    Initial Commit

---
+++ Shared/CloudNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudNoteApp.swift
+//  Shared
+//
+//  Created by Дмитрий Лисин on 07.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct CloudNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Дмитрий Лисин on 07.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
