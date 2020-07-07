66c23b2dbc2378c5bf185848a3493d1788219107
lisindima <lisindima1996@gmail.com>
2020-07-07 19:53:22 +0700

    Работа над переносом подписок

---
+++ Shared/NoteApp.swift
@@ -0,0 +1,44 @@
+//
+//  NoteApp.swift
+//  Shared
+//
+//  Created by Дмитрий Лисин on 07.07.2020.
+//
+
+import SwiftUI
+import Purchases
+
+@main
+struct NoteApp: App {
+    
+    #if !os(macOS)
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #endif
+    
+    @SceneBuilder
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        #if targetEnvironment(macCatalyst)
+        Settings {
+            SettingsView()
+        }
+        #endif
+    }
+}
+
+#if !os(macOS)
+class AppDelegate: NSObject, UIApplicationDelegate, PurchasesDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Purchases.configure(withAPIKey: "KqxRlxVJObhJFLWuNPVVPZzhckgXHjtZ")
+        Purchases.shared.delegate = self
+        return true
+    }
+    
+    func purchases(_ purchases: Purchases, didReceiveUpdated purchaserInfo: Purchases.PurchaserInfo) {
+        PurchasesStore.shared.listenPurchases()
+    }
+}
+#endif
