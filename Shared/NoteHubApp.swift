0cc56bfc8c7a2341a624a926cfc8a7f6f0ac357d
lisindima <lisindima1996@gmail.com>
2020-07-11 20:22:04 +0700

    NSApplicationDelegateAdaptor

--- Shared/NoteHubApp.swift
+++ Shared/NoteHubApp.swift
@@ -11,24 +11,44 @@ import Purchases
 @main
 struct NoteApp: App {
     
-    #if !os(macOS)
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #if os(iOS)
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    #else
+    @NSApplicationDelegateAdaptor(NSAppDelegate.self) private var appDelegate
     #endif
     
+    @SceneBuilder
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        #if os(macOS)
+        Settings {
+            SettingsView()
+        }
+        #endif
     }
 }
 
-#if !os(macOS)
-class AppDelegate: NSObject, UIApplicationDelegate, PurchasesDelegate {
-    
+#if os(iOS)
+class AppDelegate: UIResponder, UIApplicationDelegate, PurchasesDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         Purchases.configure(withAPIKey: "KqxRlxVJObhJFLWuNPVVPZzhckgXHjtZ")
+        Purchases.debugLogsEnabled = true
         Purchases.shared.delegate = self
         return true
+    }
+    
+    func purchases(_ purchases: Purchases, didReceiveUpdated purchaserInfo: Purchases.PurchaserInfo) {
+        PurchasesStore.shared.listenPurchases()
+    }
+}
+#else
+class NSAppDelegate: NSObject, NSApplicationDelegate, PurchasesDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        Purchases.configure(withAPIKey: "KqxRlxVJObhJFLWuNPVVPZzhckgXHjtZ")
+        Purchases.debugLogsEnabled = true
+        Purchases.shared.delegate = self
     }
     
     func purchases(_ purchases: Purchases, didReceiveUpdated purchaserInfo: Purchases.PurchaserInfo) {
