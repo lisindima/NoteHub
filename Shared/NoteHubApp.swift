7d17b7500a80b63feb55c984e78c56db54ff72e4
lisindima <lisindima1996@gmail.com>
2020-10-03 18:17:52 +0700

    Очистка проекта

--- Shared/NoteHubApp.swift
+++ Shared/NoteHubApp.swift
@@ -2,57 +2,16 @@
 //  NoteHubApp.swift
 //  Shared
 //
-//  Created by Дмитрий Лисин on 07.07.2020.
+//  Created by Дмитрий Лисин on 03.10.2020.
 //
 
 import SwiftUI
-import Purchases
 
 @main
-struct NoteApp: App {
-    
-    #if os(iOS)
-    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
-    #else
-    @NSApplicationDelegateAdaptor(NSAppDelegate.self) private var appDelegate
-    #endif
-    
-    @SceneBuilder
+struct NoteHubApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        #if os(macOS)
-        Settings {
-            SettingsView()
-        }
-        #endif
     }
 }
-
-#if os(iOS)
-class AppDelegate: UIResponder, UIApplicationDelegate, PurchasesDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        Purchases.configure(withAPIKey: "KqxRlxVJObhJFLWuNPVVPZzhckgXHjtZ")
-        Purchases.debugLogsEnabled = true
-        Purchases.shared.delegate = self
-        return true
-    }
-    
-    func purchases(_ purchases: Purchases, didReceiveUpdated purchaserInfo: Purchases.PurchaserInfo) {
-        PurchasesStore.shared.listenPurchases()
-    }
-}
-#else
-class NSAppDelegate: NSObject, NSApplicationDelegate, PurchasesDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        Purchases.configure(withAPIKey: "KqxRlxVJObhJFLWuNPVVPZzhckgXHjtZ")
-        Purchases.debugLogsEnabled = true
-        Purchases.shared.delegate = self
-    }
-    
-    func purchases(_ purchases: Purchases, didReceiveUpdated purchaserInfo: Purchases.PurchaserInfo) {
-        PurchasesStore.shared.listenPurchases()
-    }
-}
-#endif
