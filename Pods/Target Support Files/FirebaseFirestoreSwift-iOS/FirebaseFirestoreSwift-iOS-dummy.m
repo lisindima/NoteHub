a8fd8e5ada3854217c913603bfd1ed43f57aaeb0
lisindima <lisindima1996@gmail.com>
2020-10-04 20:02:57 +0700

    Добавил необходимые CocoaPods зависимости
    Все таки буду использовать Firestore

---
+++ Pods/Target Support Files/FirebaseFirestoreSwift-iOS/FirebaseFirestoreSwift-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FirebaseFirestoreSwift_iOS : NSObject
+@end
+@implementation PodsDummy_FirebaseFirestoreSwift_iOS
+@end
---
+++ Pods/Target Support Files/FirebaseFirestoreSwift-macOS/FirebaseFirestoreSwift-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FirebaseFirestoreSwift_macOS : NSObject
+@end
+@implementation PodsDummy_FirebaseFirestoreSwift_macOS
+@end
